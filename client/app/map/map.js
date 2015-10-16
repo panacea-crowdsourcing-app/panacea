371e7fd2717a081c4991c667766a8e16838e6af1
Yeon Seo <yseoserious@gmail.com>
2015-10-15 20:54:51 -0700

    Implement search which re-centers map to searched address location

--- client/app/map/map.js
+++ client/app/map/map.js
@@ -2,11 +2,23 @@ angular.module('panacea.map', [])
 .controller('MapController', function($scope) {
 
   var mapOptions = {
-    center: {lat: -34.055051999999996, lng: -118.44324629999998},
-    zoom: 15
+    center: {lat: 54.055051999999996, lng: -118.44324629999998},
+    zoom: 8
   };
 
   $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  $scope.geocoder = new google.maps.Geocoder();
+
+  $scope.goToLocation = function() {
+    var address = $scope.search;
+    $scope.geocoder.geocode({'address': address}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        $scope.map.setCenter(results[0].geometry.location);
+      } else {
+        alert('Geocode was not sucessful: ' + status);
+      }
+    });
+  }
 
   // Try HTML5 geolocation.
     if (navigator.geolocation) {
