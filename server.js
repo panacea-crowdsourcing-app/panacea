5a3db00c843237bc1136bd1c762fd6816f84beb2
Boshika Tara <boshikatara@gmail.com>
2015-10-19 16:08:58 -0700

    Fixed issues with server.js

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 var express = require('express')
   , io = require('socket.io').listen(server)
   , http = require('http')
-<<<<<<< HEAD
-<<<<<<< HEAD
   , twitter = require('twit')
-=======
-  , twitter = require('ntwitter')
->>>>>>> 7aab954272c5137fef5b8b4e3a80bc1e492a30a4
-=======
-  , twitter = require('twit')
->>>>>>> origin/feat/server
   , cronJob = require('cron').CronJob
   , _ = require('underscore')
   , path = require('path')
   , bodyParser = require("body-parser")
-  , AlchemyAPI = require('./server/alchemyapi')
-  , alchemyapi = new AlchemyAPI()
+  // , AlchemyAPI = require('./server/alchemyapi')
+  // , alchemyapi = new AlchemyAPI()
   , keys = require('./server/twitterKeys');
 
 
@@ -23,21 +15,13 @@ var app = express();
 
 var server = http.createServer(app);
 
-<<<<<<< HEAD
-////Instantiate the twitter component
-var t = new twitter({
-    consumer_key: 'XHt82KR3OHnFr03oVCGFal85d',
-    consumer_secret: 'gbJObOVe4yftDLT4OrspjycOGbKmoCnOepxMgVzutSCflZSMtv',
-    access_token: '2936332038-xfFZw4Qp6xIKmSX8LHF89qgwJ8IdnPt8YEwjyin',
-    access_token_secret: 'LfLh0YqH5nGCb31qJ3j1S4zDY7JaOwa6OtV75vCjpLXhp'
-=======
+
 //Instantiate the twitter component
 var t = new twitter({
     consumer_key: keys.consumer_key,
     consumer_secret: keys.consumer_secret,
-    access_token_key: keys.access_token_key,
+    access_token: keys.access_token,
     access_token_secret: keys.access_token_secret
->>>>>>> 7aab954272c5137fef5b8b4e3a80bc1e492a30a4
 });
 
 //Set the sockets.io configuration.
@@ -54,21 +38,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> origin/feat/server
+
 //Twitter symbols array
 var watchSymbols = ["malaria outbreaks", "malaria in Africa", "malaria in Asia", "parasitic disease","falciparum","ebola virus",
 "ebola outbreaks", "bird flu", "avian influenza","bird flu outbreaks","H5N1", "malaria WHO", "ebola WHO", "CDC ebola", "avian flu outbreaks", 
 "malaria symptoms", "ebola symptoms", "ebola outbreaks" ];
-<<<<<<< HEAD
-=======
-// Twitter symbols array
-var watchSymbols = ['ebola'];
->>>>>>> 7aab954272c5137fef5b8b4e3a80bc1e492a30a4
-=======
->>>>>>> origin/feat/server
+
 //This structure will keep the total number of tweets received and a map of all the symbols and how many tweets received of that symbol
 var watchList = {
     total: 0,
@@ -76,37 +51,9 @@ var watchList = {
 };
 //Set the watch symbols to zero.
 _.each(watchSymbols, function(v) { watchList.symbols[v] = 0; });
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-
-t.stream('statuses/filter', { track: watchSymbols}, function(stream) {
-  stream.on('data', function (data) {
-    console.log(data);
-  });
-});
-
-//Tisha's fiddle
-// t.stream('statuses/filter', { track: watchSymbols}, function(stream) {
-//   stream.on('data', function (data) {
-//     alchemyapi.sentiment("text", data.text, {sentiment: 1}, function(response) {
-//       console.log("Sentiment: " + response["docSentiment"]["type"]);
-//     });
-//     // console.log(data);
-//   });
-// });
->>>>>>> 7aab954272c5137fef5b8b4e3a80bc1e492a30a4
-=======
->>>>>>> origin/feat/server
-
 
 var stream = t.stream('statuses/filter', { track: watchSymbols, language: 'en', since: '2015-10-01' })
  
 stream.on('tweet', function (tweet) {
-  console.log(tweet.text + tweet.user.date + tweet.user.location);
-<<<<<<< HEAD
-})
-=======
-})
->>>>>>> origin/feat/server
+console.log(tweet.text + tweet.user.date + tweet.user.location);
+});
