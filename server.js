b909baef1077bd07ad0db7f252f1a6e282b78374
Boshika Tara <boshikatara@gmail.com>
2015-10-19 14:11:09 -0700

    Change the Twitter module to twit, also added additional keywords to the list

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
   , io = require('socket.io').listen(server)
   , http = require('http')
-  , twitter = require('ntwitter')
+  , twitter = require('twit')
   , cronJob = require('cron').CronJob
   , _ = require('underscore')
   , path = require('path')
@@ -16,7 +16,7 @@ var server = http.createServer(app);
 var t = new twitter({
     consumer_key: 'XHt82KR3OHnFr03oVCGFal85d',
     consumer_secret: 'gbJObOVe4yftDLT4OrspjycOGbKmoCnOepxMgVzutSCflZSMtv',
-    access_token_key: '2936332038-xfFZw4Qp6xIKmSX8LHF89qgwJ8IdnPt8YEwjyin',
+    access_token: '2936332038-xfFZw4Qp6xIKmSX8LHF89qgwJ8IdnPt8YEwjyin',
     access_token_secret: 'LfLh0YqH5nGCb31qJ3j1S4zDY7JaOwa6OtV75vCjpLXhp'
 });
 
@@ -34,8 +34,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Twitter symbols array
-var watchSymbols = ['ebola'];
+//Twitter symbols array
+var watchSymbols = ["malaria outbreaks", "malaria in Africa", "malaria in Asia", "parasitic disease","falciparum","ebola virus",
+"ebola outbreaks", "bird flu", "avian influenza","bird flu outbreaks","H5N1", "malaria WHO", "ebola WHO", "CDC ebola", "avian flu outbreaks", 
+"malaria symptoms", "ebola symptoms", "ebola outbreaks" ];
 //This structure will keep the total number of tweets received and a map of all the symbols and how many tweets received of that symbol
 var watchList = {
     total: 0,
@@ -45,11 +47,8 @@ var watchList = {
 _.each(watchSymbols, function(v) { watchList.symbols[v] = 0; });
 
 
-
-
-t.stream('statuses/filter', { track: watchSymbols}, function(stream) {
-  stream.on('data', function (data) {
-    console.log(data);
-  });
-});
-
+var stream = t.stream('statuses/filter', { track: watchSymbols, language: 'en', {'locations':'-180,-90,180,90'}, since: '2015-10-01' })
+ 
+stream.on('tweet', function (tweet) {
+  console.log(tweet.text + tweet.user.date);
+})
