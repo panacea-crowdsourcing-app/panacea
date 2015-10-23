67f47ac80a4f1eee8d2dffb4a645012ab92a7408
Boshika Tara <boshikatara@gmail.com>
2015-10-23 16:20:46 -0700

    Set-up neo4j in server, currently not working

--- server.js
+++ server.js
@@ -6,22 +6,23 @@ var express = require('express')
   , _ = require('underscore')
   , path = require('path')
   , bodyParser = require("body-parser") 
-  , AlchemyAPI = require('./server/alchemyapi') // Uncomment lines 9 and 10 before push
-  , alchemyapi = new AlchemyAPI()
-  , keys = require('./server/twitterKeys');
-
+  // , AlchemyAPI = require('./server/alchemyapi') // Uncomment lines 9 and 10 before push
+  // , alchemyapi = new AlchemyAPI()
+  , keys = require('./server/twitterKeys')
+  ,request = require('request')
+  ,neo4j = require('node-neo4j');
 
 var app = express();
 
 var server = http.createServer(app);
 
-//Instantiate the twitter component
-var t = new twitter({
-    consumer_key: keys.consumer_key,
-    consumer_secret: keys.consumer_secret,
-    access_token: keys.access_token,
-    access_token_secret: keys.access_token_secret
-});
+// //Instantiate the twitter component
+// var t = new twitter({
+//     consumer_key: keys.consumer_key,
+//     consumer_secret: keys.consumer_secret,
+//     access_token: keys.access_token,
+//     access_token_secret: keys.access_token_secret
+// });
 
 //Set the sockets.io configuration.
 //THIS IS NECESSARY ONLY FOR HEROKU!
@@ -55,9 +56,49 @@ var watchList = {
 //Set the watch symbols to zero.
 _.each(watchSymbols, function(v) { watchList.symbols[v] = 0; });
 
-var stream = t.stream('statuses/filter', { track: watchSymbols, language: 'en', since: '2015-10-01' });
+// var stream = t.stream('statuses/filter', { track: watchSymbols, language: 'en', since: '2015-10-01' });
  
-stream.on('tweet', function (tweet) {
-  console.log(tweet.text + tweet.user.date + tweet.user.location);
+// stream.on('tweet', function (tweet) {
+//   console.log(tweet.text + tweet.user.date + tweet.user.location);
 
-});
+// });
+
+//Define your host and port. This is where your database is running. Here it is defined on localhost.
+var host = 'localhost',
+    port = 7474;
+
+  //This is the URL where we will POST our data to fire the cypher query. This is specified in Neo4j docs.
+var httpUrlForTransaction = 'http://' + host + ':' + port + '/db/data/transaction/commit';
+
+//Create a db object. We will using this object to work on the DB.
+db = new neo4j('http://localhost:7474');
+
+//Let’s define a function which fires the cypher query.
+function runCypherQuery(query, params, callback) {
+  request.post({
+      uri: httpUrlForTransaction,
+      json: {statements: [{statement: query, parameters: params}]}
+    },
+    function (err, res, body) {
+      callback(err, body);
+    })
+}
+
+//Run raw cypher with params
+db.cypherQuery(
+  'CREATE (somebody:Person { name: {name}, from: {company}, age: {age} }) RETURN somebody',
+  {
+    name: 'somebody',
+    company: 'Modulus',
+    age: ~~(Math.random() * 100) //generate random age
+  }, function (err, result) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(result.data); // delivers an array of query results
+    console.log(result.columns); // delivers an array of names of objects getting returned
+  }
+);
+
+
+
