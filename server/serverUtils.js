329116c7022718e7656f183d1698884a0ae94234
melindab <melindab@users.noreply.github.com>
2015-10-31 16:27:27 -0500

    Add additional items to postMethod and schemas.

--- server/serverUtils.js
+++ server/serverUtils.js
@@ -11,39 +11,43 @@ var sequelize = require('./database/database.js');
 
 
   
-  var log = function(inst) {
-  console.dir(inst.get())
-}
+var log = function(inst) {
+  console.dir(inst.get());
+};
 
-  exports.postMethod = function(req, res) {
-    console.log(req.body);
+exports.postMethod = function(req, res) {
+  console.log(req.body);
   // res.json(req.body);
-  Web_SMS.create({
-    date: req.body.date,
+  Web_SMS.create({    
     persons_name: req.body.persons_name,
     email: req.body.email,
+    phone: req.body.phone,
+    diseasename: req.body.diseasename,
+    text: req.body.text,
     incidence_address: req.body.incidence_address,
     incidence_city: req.body.incidence_city,
     incidence_state: req.body.incidence_state,
+    incidence_zip: req.body.incidence_zip,
+    population: req.body.population,
     incidence_country: req.body.incidence_country,
-    diseasename: req.body.diseasename,
     no_of_cases: req.body.no_of_cases,
     latitude: req.body.latitude,
-    longitude: req.body.longitude
+    longitude: req.body.longitude,
+    date: req.body.date
   })
-  . then(function(entry) {
-    res.json(entry)
-  })
-}
+  .then(function(entry) {
+    res.json(entry);
+  });
+};
 
-  exports.getMethod = function(req, res) {
-    Web_SMS.findAll({})
-           .then(function(posts) {
-                    posts.forEach(log);
-                    res.json({posts: posts});
-                    console.log(posts);
-                   }) 
-  }
+exports.getMethod = function(req, res) {
+  Web_SMS.findAll({})
+         .then(function(posts) {
+                  posts.forEach(log);
+                  res.json({posts: posts});
+                  console.log(posts);
+                 });
+};
   
 
   
