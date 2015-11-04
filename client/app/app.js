880d73ee65b24b0dd9c8ed3cb7943b1f8bda0cc3
Yeon Seo <yseoserious@gmail.com>
2015-11-04 10:45:50 -0800

    Heatmap and Feed are populated and updated from one data source saved to rootScope

--- client/app/app.js
+++ client/app/app.js
@@ -44,21 +44,115 @@ angular.module('panacea', [
       'default': '300' // use shade 200 for default, and keep all other shades the same
     });
 })
-.controller('MainController', function($scope) {
+.controller('MainController', function($scope, $rootScope) {
   // Need to refactor to be more DRY
   $scope.toggleAbout = function() {
     $scope.about = !$scope.about;
     $scope.feed = false;
     $scope.report = false;
   };
-    $scope.toggleFeed = function() {
+  $scope.toggleFeed = function() {
     $scope.feed = !$scope.feed;
     $scope.about = false;
     $scope.report = false;
+    // NEED TO DELETE: new data gets added to $rootScope.data to see
+    // if it updates feed
+    $rootScope.data.push({
+      name: "New Disease Name ",
+      location: "Los Angeles, CA, USA",
+      coords: [27.782, -134.447],
+      time: new Date()
+     });
+    // Some kind of root function needs to be called to updated the data being
+    // used for the Google Map
+    $rootScope.update();
+    // Some kind of root function needs to be called to updated the data being
+    // used for the feed
+    $rootScope.updateFeed();
   };
     $scope.toggleReport = function() {
     $scope.report = !$scope.report;
     $scope.about = false;
     $scope.feed = false;
   };
+  $rootScope.data= [
+   {
+    name: "Disease Name 1",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -122.447],
+    time: new Date()
+   },
+   {
+    name: "Disease Name 1",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -123.447],
+    time: new Date()
+   },
+   {
+    name: "Disease Name 2",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -124.447],
+    time: new Date()
+   },
+   {
+    name: "Disease Name 2",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -125.447],
+    time: new Date()
+   },
+   {
+    name: "Disease Name 3",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -126.447],
+    time: new Date()
+   },
+   {
+    name: "Disease Name 3",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -127.447],
+    time: new Date()
+   },
+   {
+    name: "Disease Name 4",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -128.447],
+    time: new Date()
+   },
+   {
+    name: "Disease Name 4",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -129.447],
+    time: new Date()
+   },
+   {
+    name: "Disease Name 5",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -130.447],
+    time: new Date()
+   },
+   {
+    name: "Disease Name 5",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -131.447],
+    time: new Date()
+   },
+   {
+    name: "Disease Name 6",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -132.447],
+    time: new Date()
+   },
+   {
+    name: "Disease Name 6",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -133.447],
+    time: new Date()
+   },
+   {
+    name: "Disease Name 7",
+    location: "Los Angeles, CA, USA",
+    coords: [37.782, -134.447],
+    time: new Date()
+   }
+  ];
 });
--- client/app/controllers/feed.js
+++ client/app/controllers/feed.js
@@ -1,7 +1,19 @@
 angular.module('panacea.feed', [])
-.controller('FeedController', function($scope) {
-  $scope.items = [];
-          for (var i = 0; i < 50; i++) {
-            $scope.items.push(i);
-          }
+.controller('FeedController', function($scope, $rootScope) {
+  $rootScope.updateFeed = function() {
+    var reverse = $rootScope.data.slice(0);
+    $scope.items = reverse.reverse();
+  }
+  var reverse = $rootScope.data.slice(0);
+  $scope.items = reverse.reverse();
+  $scope.viewOnMap = function(name, coords){
+    $rootScope.map.setCenter({
+      lat: coords[0],
+      lng: coords[1]
+    });
+    $rootScope.userChoice = name;
+    $rootScope.changeHeatmap($rootScope.userChoice);
+    $scope.toggleFeed();
+    console.log("swag");
+  };
 });
